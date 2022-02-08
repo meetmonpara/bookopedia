1a833431f6de2e8e5fec7bcfde1c4356f02a22a7
meetmonpara <meetmonpara@gmail.com>
2022-02-08 08:58:17 +0530

    First commit

---
+++ src/main/java/com/BookopediaApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookopediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookopediaApplication.class, args);
+	}
+
+}
